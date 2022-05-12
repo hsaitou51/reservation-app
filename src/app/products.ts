0519c3107326f4b07009b0d20ff9aea47b3ea38c
hideaki <hs.wgp.46@gmail.com>
2022-05-12 23:27:29 +0900

    Angularフロントエンド側作成

---
+++ src/app/products.ts
@@ -0,0 +1,50 @@
+  export const products =[
+    {
+        covreImage: './assets/img/phone-cover.jpg',
+        name: 'Phone XL',
+        price: 799,
+        description: 'A large phone with one of the best screens',
+        heding1:'サンプルテキスト１',
+        heding2:'サンプルテキスト２',
+        heding3:'サンプルテキスト３',
+        hedingtext1: 'sampretext サンプルテキスト　sampretext サンプルテキスト sampretext サンプルテキスト　sampretext サンプルテキスト sampretext サンプルテキスト sampretext サンプルテキスト sampretext サンプルテキスト sampretext サンプルテキスト',
+        hedingtext2: 'サンプルテキスト　sampretext サンプルテキスト sampretext サンプルテキスト　sampretext サンプルテキスト sampretext サンプルテキスト sampretext サンプルテキスト sampretext サンプルテキスト sampretext サンプルテキスト sampretext',
+        hedingtext3: 'Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.'
+    },
+    {
+        covreImage: './assets/img/phone-cover.jpg',
+        name: 'Phone Mini',
+        price: 699,
+        description: 'A great phone with one fo best cameras',
+        heding1:'サンプルテキスト１',
+        heding2:'サンプルテキスト２',
+        heding3:'サンプルテキスト３',
+        hedingtext1: 'sampretext サンプルテキスト　sampretext サンプルテキスト sampretext サンプルテキスト　sampretext サンプルテキスト sampretext サンプルテキスト sampretext サンプルテキスト sampretext サンプルテキスト sampretext サンプルテキスト',
+        hedingtext2: 'サンプルテキスト　sampretext サンプルテキスト sampretext サンプルテキスト　sampretext サンプルテキスト sampretext サンプルテキスト sampretext サンプルテキスト sampretext サンプルテキスト sampretext サンプルテキスト sampretext',
+        hedingtext3: 'Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.'
+    },
+    {
+        covreImage: './assets/img/phone-cover.jpg',
+        name: 'Phone Standard',
+        price: 299,
+        description: '',
+        heding1:'サンプルテキスト１',
+        heding2:'サンプルテキスト２',
+        heding3:'サンプルテキスト３',
+        hedingtext1: 'sampretext サンプルテキスト　sampretext サンプルテキスト sampretext サンプルテキスト　sampretext サンプルテキスト sampretext サンプルテキスト sampretext サンプルテキスト sampretext サンプルテキスト sampretext サンプルテキスト',
+        hedingtext2: 'サンプルテキスト　sampretext サンプルテキスト sampretext サンプルテキスト　sampretext サンプルテキスト sampretext サンプルテキスト sampretext サンプルテキスト sampretext サンプルテキスト sampretext サンプルテキスト sampretext',
+        hedingtext3: 'Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.'
+    },
+    {
+        covreImage: './assets/img/phone-cover.jpg',
+        name: 'Phone Special',
+        price: 999,
+        description: '' ,
+        heding1:'サンプルテキスト１',
+        heding2:'サンプルテキスト２',
+        heding3:'サンプルテキスト３',
+        hedingtext1: 'sampretext サンプルテキスト　sampretext サンプルテキスト sampretext サンプルテキスト　sampretext サンプルテキスト sampretext サンプルテキスト sampretext サンプルテキスト sampretext サンプルテキスト sampretext サンプルテキスト ',
+        hedingtext2: 'サンプルテキスト　sampretext サンプルテキスト sampretext サンプルテキスト　sampretext サンプルテキスト sampretext サンプルテキスト sampretext サンプルテキスト sampretext サンプルテキスト sampretext サンプルテキスト sampretext',
+        hedingtext3: 'Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.' 
+    }
+];
