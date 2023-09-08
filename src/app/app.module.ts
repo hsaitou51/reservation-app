f88b3f7db05fc0dab1d3602cd5963e34a5cdbdb3
hideaki51 <hs.wgp.46@gmail.com>
2023-09-08 14:36:50 +0900

    Angularフロントエンド側作成

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './common/navbar/navbar.component';
@@ -8,11 +7,11 @@ import { NavbarComponent } from './common/navbar/navbar.component';
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/products.ts
@@ -0,0 +1,51 @@
+export const products = [
+    {
+      coverimge: '../../assets/img/phone-cover.jpg',
+      name: 'Phone XL',
+      price: 799,
+      description: 'A large phone with one of the best screens',
+      heaging1: '斎藤　翔太',
+      heaging2: '斎藤　陽七太',
+      heaging3: '斎藤　梓',
+      heagingtext1: 'onec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.',
+      heagingtext2: 'onec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.',
+      heagingtext3: 'onec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.'
+    },
+    {
+      coverimge: '../../assets/img/phone-cover.jpg',
+      name: 'Phone Mini',
+      price: 699,
+      description: 'A great phone with one of the best cameras',
+      heaging1: '斎藤　翔太',
+      heaging2: '斎藤　陽七太',
+      heaging3: '斎藤　梓',
+      heagingtext1: 'onec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.',
+      heagingtext2: 'onec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.',
+      heagingtext3: 'onec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.'
+    },
+    {
+      coverimge: '../../assets/img/phone-cover.jpg',
+      name: 'Phone Special',
+      price: 999,
+      description: 'A great phone with one of the best cameras',
+      heaging1: '斎藤　翔太',
+      heaging2: '斎藤　陽七太',
+      heaging3: '斎藤　梓',
+      heagingtext1: 'onec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.',
+      heagingtext2: 'onec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.',
+      heagingtext3: 'onec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.'
+      },
+    {
+      coverimge: '../../assets/img/phone-cover.jpg',
+      name: 'Phone Standard',
+      price: 299,
+      description: 'A great phone with one of the best cameras',
+      heaging1: '斎藤　翔太',
+      heaging2: '斎藤　陽七太',
+      heaging3: '斎藤　梓',
+      heagingtext1: 'onec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.',
+      heagingtext2: 'onec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.',
+      heagingtext3: 'onec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.'
+    }
+  ];
+  
